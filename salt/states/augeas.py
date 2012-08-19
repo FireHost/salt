083ce0d02270354f2639cbfa093754145d7b0dc5
Aaron Tygart <aaron.tygart@firehost.com>
2012-08-18 23:32:01 -0500

    Start work on an augeas state
    Signed-off-by: Aaron Tygart <aaron.tygart@firehost.com>

---
+++ salt/states/augeas.py
@@ -0,0 +1,40 @@
+import logging
+
+logger = logging.getlogger(__name__)
+
+
+def setvalue(value):
+    ret = {'setvalue': value,
+           'result': True,
+           'comment': ''}
+
+    if __opts__['test']:
+        ret['comment'] = 'No changes made for testing'
+        return ret
+
+    result = __salt__['augeas.setvalue'](value)
+
+    if not ['retval']:
+        ret['result'] = False
+        ret['comment'] = result['error']
+    return ret
+
+
+def remove(path):
+    ret = {'remove': path,
+           'result': True,
+           'comment': ''}
+
+    if __opts__['test']:
+        ret['comment'] = 'No changes made for testing'
+        return ret
+
+    result = __salt__['augeas.remove'](path)
+
+    if not ['retval']:
+        ret['result'] = False
+        ret['comment'] = result['error']
+        return ret
+
+    ret['comment'] = 'Changed %i lines' % result['count']
+    return ret
