e25c7b2dc94dcda7f08720d030941bd0a4121b99
Aaron Tygart <aaron.tygart@firehost.com>
2012-08-19 16:43:12 -0500

    Fix misinformation on test=True
    Signed-off-by: Aaron Tygart <aaron.tygart@firehost.com>

--- salt/states/augeas_cfg.py
+++ salt/states/augeas_cfg.py
@@ -68,8 +68,9 @@ def setvalue(name, expressions):
                                                 dfn))
         if __opts__['test']:
             if len(diff) > 0:
+                ret['changes']['diff'] = diff
                 ret['comment'] = ('Files differ, would make the following '
-                                 'changes\n%s' % diff)
+                                  'changes')
             os.remove(dfn)
         else:
             if len(diff) > 0:
